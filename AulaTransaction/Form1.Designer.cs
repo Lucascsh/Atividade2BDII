5315ffc5b06563d530c338d3bf9eb5318d685d75
Lucas Cera Schaefer <100225130+Lucascsh@users.noreply.github.com>
2022-11-09 09:23:12 -0300

    versao01

---
+++ AulaTransaction/Form1.Designer.cs
@@ -0,0 +1,135 @@
+﻿namespace AulaTransaction
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Variável de designer necessária.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Limpar os recursos que estão sendo usados.
+        /// </summary>
+        /// <param name="disposing">true se for necessário descartar os recursos gerenciados; caso contrário, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Código gerado pelo Windows Form Designer
+
+        /// <summary>
+        /// Método necessário para suporte ao Designer - não modifique 
+        /// o conteúdo deste método com o editor de código.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.tBUsuario = new System.Windows.Forms.TextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.textBox2 = new System.Windows.Forms.TextBox();
+            this.bLogin = new System.Windows.Forms.Button();
+            this.dataGridView1 = new System.Windows.Forms.DataGridView();
+            this.bConsultar = new System.Windows.Forms.Button();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // tBUsuario
+            // 
+            this.tBUsuario.Location = new System.Drawing.Point(12, 61);
+            this.tBUsuario.Name = "tBUsuario";
+            this.tBUsuario.Size = new System.Drawing.Size(100, 20);
+            this.tBUsuario.TabIndex = 0;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(12, 45);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(43, 13);
+            this.label1.TabIndex = 1;
+            this.label1.Text = "Usuario";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(12, 98);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(38, 13);
+            this.label2.TabIndex = 3;
+            this.label2.Text = "Senha";
+            // 
+            // textBox2
+            // 
+            this.textBox2.Location = new System.Drawing.Point(12, 114);
+            this.textBox2.Name = "textBox2";
+            this.textBox2.Size = new System.Drawing.Size(100, 20);
+            this.textBox2.TabIndex = 2;
+            // 
+            // bLogin
+            // 
+            this.bLogin.Location = new System.Drawing.Point(37, 153);
+            this.bLogin.Name = "bLogin";
+            this.bLogin.Size = new System.Drawing.Size(75, 23);
+            this.bLogin.TabIndex = 6;
+            this.bLogin.Text = "Login";
+            this.bLogin.UseVisualStyleBackColor = true;
+            this.bLogin.Click += new System.EventHandler(this.bLogin_Click);
+            // 
+            // dataGridView1
+            // 
+            this.dataGridView1.AllowUserToAddRows = false;
+            this.dataGridView1.AllowUserToDeleteRows = false;
+            this.dataGridView1.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dataGridView1.Location = new System.Drawing.Point(118, 12);
+            this.dataGridView1.Name = "dataGridView1";
+            this.dataGridView1.ReadOnly = true;
+            this.dataGridView1.Size = new System.Drawing.Size(358, 426);
+            this.dataGridView1.TabIndex = 7;
+            // 
+            // bConsultar
+            // 
+            this.bConsultar.Location = new System.Drawing.Point(37, 200);
+            this.bConsultar.Name = "bConsultar";
+            this.bConsultar.Size = new System.Drawing.Size(75, 23);
+            this.bConsultar.TabIndex = 8;
+            this.bConsultar.Text = "Consultar";
+            this.bConsultar.UseVisualStyleBackColor = true;
+            this.bConsultar.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(491, 450);
+            this.Controls.Add(this.bConsultar);
+            this.Controls.Add(this.dataGridView1);
+            this.Controls.Add(this.bLogin);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.textBox2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.tBUsuario);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TextBox tBUsuario;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.TextBox textBox2;
+        private System.Windows.Forms.Button bLogin;
+        private System.Windows.Forms.DataGridView dataGridView1;
+        private System.Windows.Forms.Button bConsultar;
+    }
+}
+
---
+++ AulaTransaction/Form1.cs
@@ -0,0 +1,63 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace AulaTransaction
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+            bConsultar.Enabled = false;
+        }
+
+        private void bLogin_Click(object sender, EventArgs e)
+        {
+
+            Pessoa p = new Pessoa();
+            p.nome = tBUsuario.Text;
+            Banco.setConec("Data Source=localhost;" +
+            "Initial Catalog=aula;" +
+            "User ID=" + usuario + ";" +
+            "password=123456;" +
+            "language=Portuguese");
+
+            if (p.gravar())
+            {
+                MessageBox.Show("Gravado com sucesso!");
+            }
+            else
+            {
+                MessageBox.Show("Erro ao gravar.");
+            }
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            Banco bd = new Banco();
+            DataTable dt = new DataTable();
+
+            dt = bd.executaConsulta("select * from pessoas");
+
+            dataGridView1.DataSource = dt;
+        }
+
+        private void button3_Click(object sender, EventArgs e)
+        {
+            Banco bd = new Banco();
+            DataTable dt = new DataTable();
+
+            // dt = bd.executaConsulta("select * from pessoas " +
+            //"where nome = '" + textBox4.Text + "'");
+
+            dataGridView1.DataSource = dt;
+        }
+    }
+}
